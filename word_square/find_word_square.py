b198857069522041d7b4c5a9ebb6e2cc82ece620
James Williams <zcapjwi@ucl.ac.uk>
2019-04-25 01:49:30 +0100

    passed all tests (method of ensuring no repeats still needs optimising)

--- word_square/find_word_square.py
+++ word_square/find_word_square.py
@@ -15,10 +15,11 @@ class WordList():
         @param prefix The prefix that each word must start with (does not have to be a single letter)
         @return The next word matching the criteria as a list
         '''
+        found_words = []
         def find_word():
             '''Used internally to produce a single word.'''
             word = [prefix]
-            loop_finished = True  # Assume loop will run till end
+            loop_finished = True
             for i in indices[:r]:
                 word.append(pool[i])
                 word_as_string = ''.join(word)
@@ -27,17 +28,22 @@ class WordList():
                     break
             if loop_finished and self.word_list.get(''.join(word)):
                 return word
+
         pool = list(string)
+        pool.sort()
+        last_used_letter = pool[0]
         n = len(pool)
         r = n if r is None else r
-        r = r if prefix is None else r - len(prefix)
         if r > n:
             return
+        r = r if prefix is None else r - len(prefix)
         indices = list(range(n))
         cycles = list(range(n, n - r, -1))
         word = find_word()
         if word:
-            yield word
+            if word not in found_words:
+                found_words.append(word)
+                yield word
         while n:
             for i in reversed(range(r)):
                 cycles[i] -= 1
@@ -49,7 +55,9 @@ class WordList():
                     indices[i], indices[-j] = indices[-j], indices[i]
                     word = find_word()
                     if word:
-                        yield word
+                        if word not in found_words:
+                            found_words.append(word)
+                            yield word
                     break
             else:
                 return
@@ -57,7 +65,7 @@ class WordList():
 
 if __name__ == '__main__':
     word_list = WordList('../word_lists/enable1.marisa')
-    gen = word_list.find_words('rax', 3, prefix="c")
+    gen = word_list.find_words('nabraa', 3, 'c')
     for i in gen:
         print(''.join(i))
         # print(i)
