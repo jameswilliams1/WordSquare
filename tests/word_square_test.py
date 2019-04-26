a4a4392e267232b445658d0aab420ae3d89af6b1
James Williams <zcapjwi@ucl.ac.uk>
2019-04-26 19:03:39 +0100

    completely refactored find_word into recursive solution, passes all tests

--- tests/word_square_test.py
+++ tests/word_square_test.py
@@ -6,44 +6,48 @@ import unittest
 sys.path.append('../word_square')
 from find_word_square import *
 import marisa_trie
+#import numpy as np
 
 
 class WordSquareTest(unittest.TestCase):
     def setUp(self):
         self.word_list = WordList('../word_lists/enable1.marisa')
 
-    def test_find_words_possible_answers(self):
-        word_generator = self.word_list.find_words('nabraa', 3, 'c')
+    def test_find_word_possible_answers(self):
+        word_generator = self.word_list.find_word('nabraa', 3, 'c')
         found_words = [''.join(word) for word in word_generator]
         possible_words = ['cab', 'can', 'car']
         possible_words.sort()
         found_words.sort()
         self.assertEqual(possible_words, found_words)
 
-    def test_find_words_no_possible_answers(self):
-        word_generator = self.word_list.find_words('vwj', 2, 'x')
+    def test_find_word_no_possible_answers(self):
+        word_generator = self.word_list.find_word('vwj', 2, 'x')
         with self.assertRaises(StopIteration):
             next(word_generator)
 
-    def test_find_words_r_is_1(self):
+    def test_find_word_r_is_1(self):
         # Cannot find word of length 1 when combining 2 strings
-        word_generator = self.word_list.find_words('vwb', 1, 'a')
+        word_generator = self.word_list.find_word('vwb', 1, 'a')
         with self.assertRaises(StopIteration):
             next(word_generator)
 
-    def test_find_words_r_greater_than_n(self):
+    def test_find_word_r_greater_than_n(self):
         # Desired word length > input string
-        word_generator = self.word_list.find_words('ds', 5, 'en')
+        word_generator = self.word_list.find_word('ds', 5, 'en')
         with self.assertRaises(StopIteration):
             next(word_generator)
 
-    def test_find_words_r_is_1_no_prefix(self):
-        word_generator = self.word_list.find_words('acr', 3)
+    def test_find_word_r_is_1_no_prefix(self):
+        word_generator = self.word_list.find_word('acr', 3)
         found_words = [''.join(word) for word in word_generator]
         possible_words = ['arc', 'car']
         possible_words.sort()
         found_words.sort()
         self.assertEqual(possible_words, found_words)
+
+    def test_find_word_square_possible_answers(self):
+        pass
 
 
 
--- word_square/find_word_square.py
+++ word_square/find_word_square.py
@@ -1,4 +1,6 @@
 import marisa_trie
+import sys
+from collections import Counter
 
 
 class WordList():
@@ -8,67 +10,108 @@ class WordList():
         self.word_list = marisa_trie.Trie()
         self.word_list.load(word_list_filepath)
 
-    def find_words(self, string, r=None, prefix=None):
-        '''A generator function that finds successive words matching input criteria.
-        @param string String to generate words from
-        @param r Length of each word to generate
-        @param prefix The prefix that each word must start with (does not have to be a single letter)
-        @return The next word matching the criteria as a list
-        '''
-        found_words = []
-        def find_word():
-            '''Used internally to produce a single word.'''
-            word = []
-            if prefix:
-                word.append(prefix)
-            loop_finished = True
-            for i in indices[:r]:
-                word.append(pool[i])
-                word_as_string = ''.join(word)
-                if not self.word_list.has_keys_with_prefix(word_as_string) and not self.word_list.get(word_as_string):
-                    loop_finished = False
-                    break
-            if loop_finished and self.word_list.get(''.join(word)):
-                return word
-
-        pool = list(string)
-        pool.sort()
-        n = len(pool)
-        r = n if r is None else r
-        if r > n:
+    def find_word(self, string: str, n: int, prefix: str = '') -> str:
+        '''A generator that finds one word at a time of length n from a string starting with prefix (prefix is optional).
+        @param string: The string to find words from
+        @param n: The desired length of each word
+        @param prefix: A prefix each word should start with, all words possible are output if not setUp
+        @return Successive words that are possible matching the input criteria'''
+        try:  # Skip branches where prefix has no possible items
+            next(self.word_list.iterkeys(prefix))
+        except StopIteration:
             return
-        r = r if prefix is None else r - len(prefix)
-        indices = list(range(n))
-        cycles = list(range(n, n - r, -1))
-        word = find_word()
-        if word:
-            if word not in found_words:
-                found_words.append(word)
-                yield word
-        while n:
-            for i in reversed(range(r)):
-                cycles[i] -= 1
-                if cycles[i] == 0:
-                    indices[i:] = indices[i + 1:] + indices[i:i + 1]
-                    cycles[i] = n - i
+        if len(prefix) >= n:  # Yield when word is of required length and is valid
+            if self.word_list.get(prefix):
+                yield prefix
+        else:  # Add a letter when word is too short
+            checked_letters = set()
+            for i in range(len(string)):
+                if string[i] in checked_letters: # Avoids checking the same letter combinations again
+                    continue
                 else:
-                    j = cycles[i]
-                    indices[i], indices[-j] = indices[-j], indices[i]
-                    word = find_word()
-                    if word:
-                        if word not in found_words:
-                            found_words.append(word)
-                            yield word
+                    checked_letters.add(string[i])
+                remain = string[0:i] + string[i + 1:]
+                next_word = prefix + string[i]
+                yield from self.find_word(remain, n, next_word)
+
+
+    def find_word_square(self, r, string):
+        '''Finds a valid word square of length r from string. '''
+
+        def build_square():
+            '''Used internally to add sucessive words to the word_square.'''
+
+            for i in range(1, r):
+                print(list(word_square[i]))
+                # prefix_present = all(elem in list(current_string) for elem in list(word_square[i]))
+                # if not prefix_present: # If required letters to form square not in remaining string
+                #     return
+
+                print(letters_available)
+                for letter in word_square[i]:
+                    letters_available[letter] -= 1
+                    if letters_available[letter] < 0:
+                        return
+
+                next_word = self.find_words(
+                    current_string, r, prefix=word_square[i])
+                try:
+                    word_square[i] = ''.join(next(next_word))
+                except StopIteration:
+                    return
+                for j in range(i, r - 1):  # Add the next word vertically
+                    word_square[j + 1] += word_square[i][j + 1]
+                # Remove letters in first_word from string
+                for used_letter in word_square[i]:
+                    current_string.replace(used_letter, '', 1)
+            return word_square
+
+        if not r**2 == len(string):
+            return
+        pool = list(string)
+        # Sort pool by lowest frequency
+        count = Counter(pool)
+        pool = sorted(pool, key=lambda x: count[x])
+        least_occuring = pool.pop(0)
+        pool.append(least_occuring)  # Put least occuring letter to back
+        last_used_letter = ''
+        for first_letter in pool:
+            if first_letter == last_used_letter:
+                continue
+            last_used_letter = first_letter
+            new_string = string.replace(first_letter, '', 1)
+            first_word_gen = self.find_words(
+                new_string, r, prefix=first_letter)
+            while True:
+                try:
+                    print('----------------------')
+                    current_string = new_string
+                    first_word = next(first_word_gen)
+                    current_string_list = list(current_string)
+                    # Remove letters in first_word from string
+                    for used_letter in first_word[1:]:
+                        current_string_list.remove(used_letter)
+                    letters_available = Counter(current_string_list)
+
+                    word_square = [''.join(first_word)]
+                    try:
+                        # Add first_word vertically
+                        for letter in word_square[0][1:]:
+                            word_square.append(letter)
+                            current_string_list.remove(letter)
+                    except ValueError:
+                        continue
+                    current_string = ''.join(current_string_list)
+                    print(first_word, current_string)
+                    output = build_square()
+                    if output:
+                        return output
+                except StopIteration:
                     break
-            else:
-                return
 
 
 if __name__ == '__main__':
     word_list = WordList('../word_lists/enable1.marisa')
-    gen = word_list.find_words('nabraa', 3, 'c')
-    word_generator = word_list.find_words('acr', 3)
+    gen = word_list.find_word('aaaaaaaaabbeeeeeeedddddggmmlloooonnssssrrrruvvyyy', 9, prefix='')
     for i in gen:
-        print(''.join(i))
-        # print(i)
-    # word_list.permutations("abc")
+        print(i)
